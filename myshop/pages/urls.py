d99742a4af586a1c08e3797d004610b6f6c26b50
Nikita Vertigo <ybrbnfibirjdtw@gmail.com>
2021-09-09 14:30:31 +0300

    add templates:shop, chekout, and cart

--- myshop/pages/urls.py
+++ myshop/pages/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from pages.views import *
 
 urlpatterns = [
-    path("", home_page, name="home_page")
+    path("", home_page, name="home_page"),
+    path("shop", shop_page, name="shop_page"),
+    path("cart", cart_page, name="cart_page"),
+    path("checkout", checkout_page, name="checkout_page"),
+    path("single", single_page, name="single_page")
 ]
--- myshop/pages/views.py
+++ myshop/pages/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, "home.html",{})
+
+def shop_page(request):
+    return render(request, "shop.html",{})
+
+def cart_page(request):
+    return render(request, "cart.html",{})
+
+def checkout_page(request):
+    return render(request, "checkout.html",{})
+
+def single_page(request):
+    return render(request, "single.html",{})
